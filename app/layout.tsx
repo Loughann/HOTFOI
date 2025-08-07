e919dc48d97211f9f1c6d96d02ca1e02d8ebaf6d
Loughann <loughanpk2018@gmail.com>
2025-08-06 21:45:44 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App', // Este será sobrescrito pelo page.tsx se ele tiver metadata exportada
-  description: 'Created with v0',
+  title: 'Privacy Luana Vieira', // Este será sobrescrito pelo page.tsx se ele tiver metadata exportada
+  description: 'Luana Vieira - Perfil oficial',
   generator: 'v0.dev',
 }
 
